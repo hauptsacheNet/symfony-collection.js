94e3725e4e646ed8621493e7ce33af67e52dd5f9
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2014-10-14 16:45:47 +0200

    Sanitize code base
    enforced semicolons at eol
    if blocks always within braces
    code formatting

--- symfony-collection.js
+++ symfony-collection.js
@@ -16,20 +16,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================= */
-  !function( $ ){
+!function($) {
 
-    "use strict";
+  "use strict";
 
- /* COLLECTION CLASS DEFINITION
-  * =========================== */
+  /* COLLECTION CLASS DEFINITION
+   * =========================== */
 
-  var Collection = function (element, options) {
-    this.options = options
-    this.elementId = '#' + element.id
+  var Collection = function(element, options) {
+    this.options = options;
+    this.elementId = '#' + element.id;
     this.$element = $(element)
-      .delegate('[data-remove="collection"]', 'click.remove.collection', $.proxy(this.remove, this))
+      .delegate('[data-remove="collection"]', 'click.remove.collection', $.proxy(this.remove, this));
 
-    this.usesLi = this.$element.is('ul')
+    this.usesLi = this.$element.is('ul');
 
     if (this.usesLi) {
       this.count = $(this.elementId + ' > li').length
@@ -52,77 +52,76 @@
 
   Collection.prototype = {
 
-      constructor: Collection
+    constructor: Collection, add: function() {
+      var newElement = this.options.prototype;
 
-    , add: function() {
-        var newElement = this.options.prototype;
+      if (this.options.limit && this.count >= this.options.limit) {
+        return;
+      }
 
-        if (this.options.limit && this.count >= this.options.limit) return
+      newElement = newElement.replace(/__name__/g, this.options.index);
 
-        newElement = newElement.replace(/__name__/g, this.options.index);
+      if (this.usesLi) {
+        newElement = $('<li></li>').html(newElement);
+      }
 
+      this.$element.append(newElement);
+
+      this.options.index++;
+      this.count++;
+    }, remove: function(e) {
+      var $target = $(e.target);
+
+      e && e.preventDefault();
+
+      if ($target.parents(this.elementId).length !== 0) {
         if (this.usesLi) {
-          newElement = $('<li></li>').html(newElement);
+          $target.closest(this.elementId + ' > li').remove();
+        } else {
+          $target.closest(this.elementId + ' > div').remove();
         }
-
-        this.$element.append(newElement);
-
-        this.options.index++
-        this.count++
       }
 
-    , remove: function(e) {
-        var $target = $(e.target)
-
-        e && e.preventDefault()
-
-        if ($target.parents(this.elementId).length !== 0) {
-          if (this.usesLi) {
-            $target.closest(this.elementId + ' > li').remove();
-          } else {
-            $target.closest(this.elementId + ' > div').remove();
-          }
-        }
-
-        this.count--
-      }
-
-    , isInt: function(value) {
-        return typeof value === 'number' && value % 1 == 0
-      }
+      this.count--
+    }, isInt: function(value) {
+      return typeof value === 'number' && value % 1 == 0
+    }
   };
 
- /* COLLECTION PLUGIN DEFINITION
-  * ============================ */
+  /* COLLECTION PLUGIN DEFINITION
+   * ============================ */
 
-  $.fn.collection = function (option) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('collection')
-        , options = $.extend({}, $.fn.collection.defaults, $this.data(), typeof option == 'object' && option)
-      if (!data) $this.data('collection', (data = new Collection(this, options)))
-      if (typeof option == 'string') data[option]()
+  $.fn.collection = function(option) {
+    return this.each(function() {
+      var $this = $(this), data = $this.data('collection'), options = $.extend({}, $.fn.collection.defaults, $this.data(), typeof option == 'object' && option);
+      if (!data) {
+        $this.data('collection', (data = new Collection(this, options)))
+      }
+
+      if (typeof option == 'string') {
+        data[option]();
+      }
     })
-  }
+  };
 
   $.fn.collection.defaults = {
-      limit: null,
-      index: 0
-  }
+    limit: null,
+    index: 0
+  };
 
-  $.fn.collection.Constructor = Collection
+  $.fn.collection.Constructor = Collection;
 
+  /* COLLECTION DATA-API
+   * =================== */
 
- /* COLLECTION DATA-API
-  * =================== */
+  $(function() {
+    $('body').on('click.collection.data-api', '[data-add="collection"]', function(e) {
+      var
+        $this = $(this),
+        href = $this.attr('href'),
+        $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))); //strip for ie7
 
-  $(function () {
-    $('body').on('click.collection.data-api', '[data-add="collection"]', function ( e ) {
-      var $this = $(this)
-      , href = $this.attr('href')
-      , $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) //strip for ie7
-
-      e.preventDefault()
+      e.preventDefault();
 
       $target.collection('add')
     });
@@ -130,4 +129,4 @@
       $(this).collection()
     });
   })
-}(window.jQuery)
+}(window.jQuery);
